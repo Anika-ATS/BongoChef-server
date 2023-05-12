4cc4189e7b687d50186e69edce57166f411dcdc1
Anika <tabassumanika210@gmail.com>
2023-05-12 14:52:03 +0600

    All recipe of individual chef

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT||5000;
 const  chefData =require('./data/chefData.json');
+const  details=require('./data/ChefDetails.json');
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -12,6 +13,19 @@ app.get('/', (req, res) => {
   app.get('/chefData', (req, res) => {
     res.send(chefData);
   })
+  //data getting
+  app.get('/details', (req, res) => {
+    res.send(details);
+  })
+  //with id
+  app.get('/details/:id', (req, res) => {
+    // res.send(details);
+    const id=req.params.id;
+    console.log(id);
+    const selectedRecipeDetails=details.find(n=>n.id==id);
+    res.send(selectedRecipeDetails)
+  })
+
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
